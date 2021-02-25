dff012509a4d748ecd2e32bc0dc39e437f6303e6
chill-phil <78041889+chill-phil@users.noreply.github.com>
2021-02-24 20:00:17 -0600

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,14 +3,12 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto","Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+// fight functon
+var fight = function(enemyName) {
    // Alert players that they are starting the round
    window.alert("Welcome to Robot Gladiators!");
 
@@ -54,7 +52,7 @@ var fight = function() {
                 window.alert(playerName + " has decided to skip this fight. Goodbye!");
     
             // subtract money from playerMoney for skipping
-            playerMoney = playerMoney - 2;
+                playerMoney = playerMoney - 2;
             }
   
             // if no (false), ask question again by running fight() again
@@ -62,9 +60,18 @@ var fight = function() {
                 fight();
             }
 
-        } else {
-            window.alert("You need to choose a valid option. Try again!");
+            // if player did not choose 1 or 2 in prompt
+        }   else {
+                window.alert("You need to choose a valid option. Try again!");
         }
     }
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
+
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
